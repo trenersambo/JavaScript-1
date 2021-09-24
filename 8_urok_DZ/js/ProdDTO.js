dca060570ecffa037172a2b75f5c5a4280383c4b
trenersambo <arb.pfr@gmail.com>
2021-09-24 20:38:31 +0300

    DZ_8_cart

---
+++ 8_urok_DZ/js/ProdDTO.js
@@ -0,0 +1,9 @@
+class ProdDTO{
+    constructor(id, img, name, opisanie, price){
+        this.id = id;
+        this.img = img;
+        this.name = name;
+        this.opisanie = opisanie;
+        this.price = price;
+    }
+}
---
+++ 8_urok_DZ/js/prods.js
@@ -0,0 +1,28 @@
+const prods = [
+    
+new ProdDTO(
+    0,
+    'pf.png',
+    'Tovar_0',
+    'God Product for sambo',
+    400,
+),
+
+new ProdDTO(
+    1,
+    'pf.png',
+    'Tovar_1',
+    'God Product for judo',
+    500,
+),
+
+new ProdDTO(
+    2,
+    'pf.png',
+    'Tovar_2',
+    'God Product for BJJ',
+    600,
+),
+
+
+];
