8b232a0f80ddd5c5abec9b081a257fcb64f963fa
trenersambo <arb.pfr@gmail.com>
2021-09-24 20:41:27 +0300

    DZ_8_cart

--- 8_urok_DZ/js/basket.js
+++
@@ -1,9 +0,0 @@
-/* При клике на КОРЗИНУ: появляется/исчезает сводТаблица */
-const openBasket = document.querySelector('.korz_icon');
-const basketEl = document.querySelector('.basket');
-
-openBasket.addEventListener('click' , function(){
-    basketEl.classList.toggle('hidden');
-});
-
-
