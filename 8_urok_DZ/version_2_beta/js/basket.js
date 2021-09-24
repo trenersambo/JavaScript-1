fe45dd2d3391e3c329cdf23ba855530b3b9d1f41
trenersambo <arb.pfr@gmail.com>
2021-09-24 20:44:25 +0300

    DZ_8_cart

---
+++ 8_urok_DZ/version_2_beta/js/basket.js
@@ -0,0 +1,102 @@
+/* При клике на КОРЗИНУ: появляется/исчезает сводТаблица */
+const openBasket = document.querySelector('.korz_icon');
+const basketEl = document.querySelector('.basket');
+
+openBasket.addEventListener('click' , function(){
+    basketEl.classList.toggle('hidden');
+});
+
+/* ------------ */
+
+/*Ф-ция: Слушает клики кнопки Add To cart*/
+function clickAddToCart(){
+    const btnId = document.querySelectorAll('button[data-productId]');
+    btnId.forEach (function (button){
+        button.addEventListener('click' , adProdHand);
+    })
+};
+
+/*Ф-ция, кот.обрабатывает событие от клика  */
+function adProdHand (event){
+    const prodId = event.currentTarget.getAttribute('data-productId');
+
+    itogKorzina (prodId);
+}
+
+clickAddToCart ();
+
+
+/*найти span из иконки Корзины */
+const basketCountEl = document.querySelector('.korz_icon span');
+
+/* найти 2 класса из сводТаблицы */
+const basketTotalEl = document.querySelector('.basketTotal');//СтрокаИтогоСуммаВсехТоваров
+const basketTotalValueEl = document.querySelector('.basketTotalValue');//ЦифраИтогоСуммаВсехТоваров
+
+/* 1. внутрь объекта basket прилетает product c "ключ:значение" */
+let basket = {};
+
+/* 2. Этот прилет обеспечивает функция/метод */
+function prodToKorzina (prodId){
+
+    //если нет id, то присвоить 1, иначе ++
+    if(!(prodId in basket)){
+        basket[prodId] = 1;
+    } else {
+        basket[prodId]++ ;
+    }
+}
+
+/* 3. Функ-я подготовки отрисовки полученных данных в сводТаблице */
+function  vstavkaInKorzina(prodId){
+    let prodEx = document.querySelector (`.prodCount[data-prodId = "${prodId}"]`);
+
+    // если найден класс .prodCount
+    if (prodEx) {
+        plusProdCount(prodId);//увеличЧислоТоваровВКорзине (см.5)
+        plusProdSum(prodId);// пересчетИтогСуммыПоТовару (см.6)
+        }else{
+            korzinaNewStroka(prodId);
+        }
+}
+ 
+/*4. Ф-ия рисует новые СТРОКИ в сводТаблице */
+
+function korzinaNewStroka(prodId) {
+
+    let vstavkaInTable= `
+    
+    <div class="basketRow">
+        <div> ${prods[prodId].name} </div> <!--Жду: Название товара-->
+        <div> <span class = "prodCount" data-prodId = "${prodId}"> 1 </span> штук </div> <!--Жду: Количество-->
+        <div> $${prods[prodId].price} </div> <!--Жду: Цена за шт.-->
+        <div>  $<span class = "prodTotalRow" data-prodId = "${prodId}"> ${prods[prodId].price}</span> </div> <!--Жду: Итого-->
+    </div>
+    
+    ` 
+     basketTotalEl.insertAdjacentHTML("beforebegin", vstavkaInTable);
+
+}
+
+/*5. функ-ия увеличит кол-во товаров в строке в корзине */
+
+function plusProdCount (prodId){
+    const prodCountEl = document.querySelector (`.prodCount[data-prodId = "${prodId}"]`);
+    prodCountEl.textContent++;
+}
+
+/* 6. Ф-ия пересчит.: ЦенаТовара*КоличТовара */
+function plusProdSum (prodId){
+    const prodTotalRowEl = document.querySelector(`.prodTotalRow[datd-prodId = "${prodId}"]`);
+    let totalPriceForRow = (basket[prodId] * prods[prodId].price).toFixed(2); //штук*цена
+    prodTotalRowEl.textContent = totalPriceForRow;
+}
+
+
+/*7. Считаем Общую стоимость Корзины. Вывод цены на страницу */
+
+function itogSum (){
+    let totalSum = 0;
+    
+}
+ 
